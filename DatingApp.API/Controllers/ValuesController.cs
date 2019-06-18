75695bd301e916e264326cfbede16a080ee01ccd
Jolanta Serafin <szyszjola@gmail.com>
2019-06-18 18:19:00 +0200

    Init

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    //GET http://localhost:5000/api/values/5
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ValuesController(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult>  GetValues()
+        {
+            var values = await _dataContext.Values.ToListAsync();
+            return Ok(values);
+        }
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _dataContext.Values.FirstOrDefaultAsync(x => x.Id == id); //default -- null
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
